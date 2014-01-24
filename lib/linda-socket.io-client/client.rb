7bc4ec456220b2e205bb28de774e36a96a3b715d
Sho Hashimoto <hashimoto@shokai.org>
2014-01-24 21:32:10 +0900

    remove Socket.IO callbacks on IO closed  #3

--- lib/linda-socket.io-client/client.rb
+++ lib/linda-socket.io-client/client.rb
@@ -39,6 +39,27 @@ module Linda
           @name = name
           @linda = linda
           @watch_callback_ids = {}
+          @io_callback_ids = []
+          this = self
+          @linda.io.on 'disconnect' do
+            this.remove_io_callbacks
+          end
+        end
+
+        def create_callback_id
+          "#{Time.now.to_i}_#{rand 10000}"
+        end
+
+        def create_watch_callback_id(tuple)
+          key = tuple.to_json
+          return @watch_callback_ids[key] ||= create_callback_id
+        end
+
+        def remove_io_callbacks
+          @io_callback_ids.each do |id|
+            @linda.io.remove_listener id
+          end
+          @io_callback_ids = []
         end
 
         def write(tuple, opts={})
@@ -53,6 +74,7 @@ module Linda
           io_cid = @linda.io.once name do |err, tuple|
             block.call err, Tuple.new(tuple)
           end
+          @io_callback_ids.push io_cid
           @linda.io.emit '__linda_take', {:tuplespace => @name, :tuple => tuple, :id => id}
           return id
         end
@@ -64,6 +86,7 @@ module Linda
           io_cid = @linda.io.once name do |err, tuple|
             block.call err, Tuple.new(tuple)
           end
+          @io_callback_ids.push io_cid
           @linda.io.emit '__linda_read', {:tuplespace => @name, :tuple => tuple, :id => id}
           return id
         end
@@ -75,22 +98,13 @@ module Linda
           io_cid = @linda.io.on name do |err, tuple|
             block.call err, Tuple.new(tuple)
           end
+          @io_callback_ids.push io_cid
           @linda.io.emit '__linda_watch', {:tuplespace => @name, :tuple => tuple, :id => id}
           return id
         end
 
         def cancel(id)
           @linda.io.emit '__linda_cancel', {:tuplespace => @name, :id => id}
-        end
-
-        private
-        def create_callback_id
-          "#{Time.now.to_i}_#{rand 10000}"
-        end
-
-        def create_watch_callback_id(tuple)
-          key = tuple.to_json
-          return @watch_callback_ids[key] ||= create_callback_id
         end
 
       end
